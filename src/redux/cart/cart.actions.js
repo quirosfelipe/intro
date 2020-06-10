8ee0a1cef5681d5a1bf3ac533041b373685c5db4
Felipe Quiros <quirosfelipe@gmail.com>
2020-06-10 18:05:23 -0400

    add item , remove item and delet item added to cart functionality

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,11 +1,21 @@
 import CartActionTypes from "./cart.types";
 
-export const toggleCartHidden = () => ({
-  // no payload needed
-  type: CartActionTypes.TOGGLE_CART_HIDDEN,
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
 });
 
 export const addItem = (item) => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item,
+});
+
+export const removeItem = (item) => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item,
+});
+
+export const toggleCartHidden = () => ({
+  // no payload needed
+  type: CartActionTypes.TOGGLE_CART_HIDDEN,
 });
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,3 +11,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  // IF THERE IS ONLY ONE ITEM QTY
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  //IF THERE IS MORE THAN ONE QTY ITEM
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
