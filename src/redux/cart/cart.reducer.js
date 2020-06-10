199cb365b14e2694b6b02c650baea15adf260317
Felipe Quiros <quirosfelipe@gmail.com>
2020-06-10 12:29:41 -0400

    cart dropdown and cart items connected

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from "./cart.types";
-
+import { addItemToCart } from "./cart.utils";
 const INITIAL_STATE = {
   hidden: true,
   cartItems: [],
@@ -15,7 +15,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
       return state;
